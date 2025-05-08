13babab66f54e577247a6b584230f90fae00bba8
olgatenison <88651744+olgatenison@users.noreply.github.com>
2025-05-09 01:11:29 +0300

    locales

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,27 +1,24 @@
-// app/[locale]/layout.js
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { setRequestLocale } from "next-intl/server";
-import { routing } from "../../i18n/routing";
+import { routing } from "@/i18n/routing";
 
 export function generateStaticParams() {
-  // Указываем, что нужно сгенерировать страницы для каждой локали
   return routing.locales.map((locale) => ({ locale }));
 }
 
-export default function LocaleLayout({ children, params }) {
-  const { locale } = params;
-  // Проверяем валидность локали, иначе 404
+export default async function LocaleLayout({ children, params }) {
+  const { locale } = await params;
+
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
-  // Фиксируем локаль запроса для next-intl (включаем режим SSG)
+
   setRequestLocale(locale);
 
   return (
     <html lang={locale}>
       <body>
-        {/* Провайдер передает переводы и текущую локаль в дочерние компоненты */}
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
     </html>
